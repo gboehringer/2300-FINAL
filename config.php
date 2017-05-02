584ef44b5124538976890f4a316d0b8f9aa6fecb
Juan Garcia <jg845@cornell.edu>
2017-05-02 17:20:15 -0400

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,7 @@
     define('DB_HOST','localhost');
     
     // name, user, and password are same for local and course server.
-  //  define('DB_NAME','info230_SP17_fp_big_red_w');
-   // define('DB_USER','fp_big_red_w');
-    //define('DB_PASSWORD','PassSP172300');
-        define('DB_NAME','info230_SP17_jg845sp17');
-    define('DB_USER','jg845sp17');
-    define('DB_PASSWORD','Oiuyt214');
+    define('DB_NAME','info230_SP17_fp_big_red_w');
+    define('DB_USER','fp_big_red_w');
+    define('DB_PASSWORD','PassSP172300');
 ?>
