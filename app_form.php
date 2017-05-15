81af892c4a1e7df5f9b5485a45d4941814250ba6
Daniel Gartshein <dg637@cornell.edu>
2017-05-15 19:03:47 -0400

    Add files via upload
    Finished application form with validation

--- app_form.php
+++ app_form.php
@@ -33,10 +33,10 @@
 				<input type = "text" name = "firstname" placeholder = "First Name" required>
 				<input type = "text" name = "middlename" placeholder = "Middle Name - Optional">
 				<input type = "text" name = "lastname" placeholder = "Last Name" required>
-				<input type = "text" name = "major" placeholder = "Major" >
+				<input type = "text" name = "major" placeholder = "Major" required>
 				<input type = "text" name = "net_id" placeholder = "Net ID" required>
 				<input type = "text" name = "year" placeholder = "Graduation Year" required>
-				<h3>Resume Upload</h3>
+				<h3>Resume Upload (.pdf or .doc)</h3>
 				<input type = 'file' name = 'resume' required>
 				<h3>Headshot Upload</h3>
 				<input type = 'file' name = 'headshot' >
@@ -54,11 +54,21 @@
                 die("Connection failed: " . $mysqli->connect_error);
                 }
 
+			function validate($data) {
+				$data = trim($data);
+				$data = stripslashes($data);
+				$data = htmlspecialchars($data);
+				return $data;
+			}        
+
+		// Checks if form was submitted
  		if (isset($_POST['submit'])) {
+ 			//Validates that file is an image
  			if (!empty($_FILES['headshot']['tmp_name']) && !($_FILES['headshot']['error'] >0) && ($_FILES['headshot']['size'] < 5242880)) {
-            	$info = getimagesize($_FILES['headshot']['tmp_name']);
-            	if ($info ===FALSE){
-                	echo "Invalid File Type";
+         
+            	if (@getimagesize($_FILES['headshot']['tmp_name']) ===FALSE){
+                	die("Invalid File Type");
+                	return;
             	}	
             	else{
     		    	$newImage = $_FILES['headshot'];
@@ -66,56 +76,85 @@
     		    	$headshotName = $newImage['name'];
     		    	$headshotName = filter_var($headshotName, FILTER_SANITIZE_URL);
 			    	move_uploaded_file($tmp, "applicants/headshots/$headshotName");
-			}
+				}
 
+			//Validates that file is a pdf or doc 
  			if (!empty($_FILES['resume']['tmp_name']) && !($_FILES['resume']['error'] >0) && ($_FILES['resume']['size'] < 5242880)) {
-            
-            
-    		    $newImage = $_FILES['resume'];
-                $tmp = $newImage['tmp_name'];
-    		    $resumeName = $newImage['name'];
-    		    $resumeName = filter_var($resumeName, FILTER_SANITIZE_URL);
-			    move_uploaded_file($tmp, "applicants/resumes/$resumeName");
-
-
+ 					$finfo = finfo_open(FILEINFO_MIME_TYPE);
+					$mime = finfo_file($finfo, $_FILES['resume']['tmp_name']);
+					switch ($mime) {
+						case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+							break;
+   						case 'application/doc':
+   							break;
+   						case 'application/pdf':
+   							break;
+               			default:
+                			die("Invalid File Type");
+                			return;
+            	}	          
+    		    	$newImage = $_FILES['resume'];
+               		$tmp = $newImage['tmp_name'];
+    		    	$resumeName = $newImage['name'];
+    		    	$resumeName = filter_var($resumeName, FILTER_SANITIZE_URL);
+			    	move_uploaded_file($tmp, "applicants/resumes/$resumeName");
+				
+			//Validation for net_id
             if (!empty($_POST['net_id'])){
     			if (ctype_alnum($_POST['net_id'])){
-                	$net = $_POST['net_id'];
+                	$net = validate($_POST['net_id']);
             	}
        		}
 
+       		//validation for first name
             if (!empty($_POST['firstname'])){
     			if (ctype_alpha($_POST['firstname'])){
-                	$firstname = $_POST['firstname'];
+                	$firstname = validate($_POST['firstname']);
             	}
+            	else{
+            		echo ("Please enter a valid first name");
+            		return;
+            	}            	
        		}
 
+       		//validation for middle name
             if (!empty($_POST['middlename'])){
     			if (ctype_alpha($_POST['middlename'])){
-                	$middlename = $_POST['middlename'];
+                	$middlename = validate($_POST['middlename']);
             	}
         	}
-        	else{
-            	$middlename = "N/A";
+        		else{
+            		$middlename = "N/A";
             	}
-
+			//validation for major
             if (!empty($_POST['major'])){
-    			if (ctype_alpha($_POST['major'])){
-                	$major = $_POST['major'];
+    			if (preg_match('/^[a-zA-Z\s]+$/', $_POST['major'])){
+                	$major = validate($_POST['major']);
             }
-            else{
-            	$major = "No Major Listed";
+             	else{
+            		echo ("Please enter a valid major or 'Undecided'");
+            		return;
             	}
-        	}        	
+        	}   
 
+        	//validation for last name
             if (!empty($_POST['lastname'])){
     			if (ctype_alpha($_POST['lastname'])){
-                	$lastname = $_POST['lastname'];
+                	$lastname = validate($_POST['lastname']);
+            	}
+            	else{
+            		echo ("Please enter a valid last name");
+            		return;
             	}
         	}
+        	//validation for year
             if (!empty($_POST['year'])){
-    			if (ctype_alnum($_POST['year'])){
-                	$year = $_POST['year'];
+    			if (ctype_digit($_POST['year'])){
+                	$year = validate($_POST['year']);
+            	}
+            	else{
+            		echo ("Invalid graduation year entered");
+            		return;
             	}
         	}
 
@@ -123,21 +162,18 @@
         	$pathheadshot = "applicants/headshots/$headshotName";	
         	$pathresume = "applicants/resumes/$resumeName";	
         	$add = "INSERT INTO Applicants (net_id,firstName,lastName,middleName,grad_year,major,date_applied,headshot_path,resume_path) VALUES('$net','$firstname','$lastname','$middlename','$year','$major','$date','$pathheadshot','$pathresume')";
-
+        	//Inserts data into Applicants Database
         	if($mysqli->query($add)){
         		$app = mysqli_insert_id($mysqli);
         		echo ("<h3>Application Submitted</h3>");
         	}
+        	else{
+        		echo("Invalid Entry. Please resubmit your application.");
+        	}
         }
         }
 
-
-			function validate($data) {
-				$data = trim($data);
-				$data = stripslashes($data);
-				$data = htmlspecialchars($data);
-				return $data;
-			}        	
+	
 
 		}
 
