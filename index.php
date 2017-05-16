7feefa6c1383fd76e653f3e3a43660d287b51863
Gillian Grace Boehringer <ggb39@cornell.edu>
2017-05-15 23:38:04 -0400

    changed latin to english

--- index.php
+++ index.php
@@ -125,7 +125,7 @@
 					<div class="col-lg-12">
 						<h2>Become A Member</h2>
 						<a href = "app_form.php"><button id = "app_button">Apply</button></a>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Fall recruitment starts soon! Apply to become a member of our club and we'll get back to you as soon as we can.</p>
 					</div>
 				</div>
 			</div>
@@ -134,7 +134,7 @@
 				<div class="row">
 					<div class="col-lg-12">
 						<h2>Contact Us</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Send us a message if you're interested in working with us or just want to find out more!</p>
 					</div>
 					<div id="contact_div" class="container-fluid section">
 						<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" id="contact_form">
